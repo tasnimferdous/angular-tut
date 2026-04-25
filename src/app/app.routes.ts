188b696d1d023b38e84b9bc249f45b3901425675
tasnimferdous <tasnimferdous216@gmail.com>
2026-04-26 04:28:11 +0600

    Routing and Header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { Login } from './login/login';
 import { About } from './about/about';
 import { Contact } from './contact/contact';
+import { Home } from './home/home';
 
 export const routes: Routes = [
+    {path:"", component:Home},
     {path:"login", component:Login},
     {path:"about", component:About},
     {path:"contact", component:Contact}
