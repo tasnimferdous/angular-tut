e6100000eaf53ecb38907773a0028de7c47872a8
tasnimferdous <tasnimferdous216@gmail.com>
2026-05-02 23:50:02 +0600

    dynamic routing

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-
+  users = [
+    { id: 1, name: 'Alice' },
+    { id: 2, name: 'Bob' },
+    { id: 3, name: 'Charlie' }
+  ];
 }
---
+++ src/app/user/user.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-user',
+  imports: [],
+  templateUrl: './user.html',
+  styleUrl: './user.css',
+})
+export class User {
+  userName: string = '';
+  constructor(private route: ActivatedRoute) {
+
+  }
+
+  ngOnInit() {
+    this.route.params.subscribe((params) => {
+      console.log(params);
+      this.userName = "a";
+    });
+  }
+}
