b3c8c66dd3f2112ed29f6846fca62f06ec5c4057
tasnimferdous <tasnimferdous216@gmail.com>
2026-05-03 11:24:28 +0600

    dynamic routing bug fixed

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,18 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, RouterLink],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  users = [
-    { id: 1, name: 'Alice' },
-    { id: 2, name: 'Bob' },
-    { id: 3, name: 'Charlie' }
-  ];
+
 }
--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home {
+    users = [
+    { id: 1, name: 'Alice' },
+    { id: 2, name: 'Bob' },
+    { id: 3, name: 'Charlie' }
+  ];
+}
